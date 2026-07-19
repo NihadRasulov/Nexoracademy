8046fdc9574bccf18cd54a342cba9d47e8e67162
NihadRasulov <nihatrasulov1207@gmail.com>
2026-07-19 13:13:55 +0400

    first change

---
+++ src/main/java/az/demo/NexoraAcademy/NexoraAcademyApplication.java
@@ -0,0 +1,13 @@
+package az.demo.NexoraAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoraAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/demo/NexoraAcademy/NexoraAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package az.demo.NexoraAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoraAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
